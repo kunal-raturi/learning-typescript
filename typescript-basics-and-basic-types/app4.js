4d47b8a5cd002fc2683322bf6bf954d1f7b88689
kunal <kunalraturi1@gmail.com>
2024-01-28 20:51:13 +0530

    object type in typeScript

---
+++ typescript-basics-and-basic-types/app4.js
@@ -0,0 +1,6 @@
+var student = {
+    name: "aik",
+    age: 12,
+    isStudent: false,
+};
+console.log(student.name);
---
+++ typescript-basics-and-basic-types/app4.ts
@@ -0,0 +1,13 @@
+type Person = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
+
+const student: Person = {
+  name: "aik",
+  age: 12,
+  isStudent: false,
+};
+
+console.log(student.name);
