a83e2fa30c435c008ff01c6ab90af5d44606b9b0
kunal <kunalraturi1@gmail.com>
2024-01-30 14:21:34 +0530

    working with enum in ts

--- typescript-basics-and-basic-types/app6.js
+++ typescript-basics-and-basic-types/app6.js
@@ -1,7 +1,18 @@
+var interest;
+(function (interest) {
+    interest[interest["cricket"] = 0] = "cricket";
+    interest[interest["cooking"] = 1] = "cooking";
+    interest[interest["singing"] = 2] = "singing";
+    interest[interest["flowering"] = 3] = "flowering";
+})(interest || (interest = {}));
 var employee = {
     name: "ali",
     age: 23,
     role: ["engineer", 2],
+    Hobby: interest.singing,
 };
-employee.role.push("marketing");
+// employee.role.push("marketing");
 // console.log(employee.role[1]);
+if (employee.Hobby === interest.singing) {
+    console.log("employee hobby is singing");
+}
--- typescript-basics-and-basic-types/app6.ts
+++ typescript-basics-and-basic-types/app6.ts
@@ -2,12 +2,23 @@ type Person = {
   name: string;
   age: number;
   role: [string, number];
+  Hobby: interest;
 };
 
+enum interest {
+  cricket, //automatically asign value 0 if not declare
+  cooking,
+  singing,
+  flowering,
+}
 const employee: Person = {
   name: "ali",
   age: 23,
   role: ["engineer", 2],
+  Hobby: interest.singing,
 };
-employee.role.push("marketing");
+// employee.role.push("marketing");
 // console.log(employee.role[1]);
+if (employee.Hobby === interest.singing) {
+  console.log("employee hobby is singing");
+}
