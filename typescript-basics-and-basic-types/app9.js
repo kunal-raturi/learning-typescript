ea8c5a6f46ce328237831d6bea7eb25400ab2da0
kunal <kunalraturi1@gmail.com>
2024-02-07 10:50:37 +0530

    aliases type

---
+++ typescript-basics-and-basic-types/app9.js
@@ -0,0 +1,4 @@
+var id = 12345;
+console.log(id);
+var origina = { x: 0, y: 0 };
+console.log(origina);
---
+++ typescript-basics-and-basic-types/app9.ts
@@ -0,0 +1,11 @@
+type UserId = number;
+let id: UserId = 12345;
+console.log(id);
+
+type point = {
+  x: number;
+  y: number;
+};
+
+let origina: point = { x: 0, y: 0 };
+console.log(origina);
